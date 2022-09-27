2d68bb504e48226cc25fd39e3aeada3e1844e469
Naomi-Vega <97486808+Naomi-Vega@users.noreply.github.com>
2022-09-27 16:17:38 -0400

    improved styling

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,38 +1,43 @@
 import './App.css';
+import { FaUserAlt } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <h1>Sign Up</h1>
-        <p>
-          It's free and only takes a minute
-        </p>
-      </header>
-      <div className="login-section">
-        <label for="first-name"><p>First Name</p></label>
-        <input type="text" name="fistname" required />
+      <div className="container">
+        <div className="header">
+          <h1>Sign Up</h1>
+          <p>
+            It's free and only takes a minute
+          </p>
+        </div>
+        <div className="login-section">
+          <label for="first-name"><p>First Name</p></label>
+          <input type="text" name="fistname" required />
 
-        <label for="last-name"><p>Last Name</p></label>
-        <input type="text" name="lastname" required />
+          <label for="last-name"><p>Last Name</p></label>
+          <input type="text" name="lastname" required />
 
-        <label for="email"><p>Email</p></label>
-        <input type="text" name="user-email" required />
+          <label for="email"><p>Email</p></label>
+          <input type="text" name="user-email" required />
 
-        <label for="password"><p>Password</p></label>
-        <input type="password" name="user-pword" required />
+          <label for="password"><p>Password</p></label>
+          <input type="password" name="user-pword" required />
 
-        <label for="confirm-password"><p>Confirm Password</p></label>
-        <input type="password" name="confirm-pword" required />
+          <label for="confirm-password"><p>Confirm Password</p></label>
+          <input type="password" name="confirm-pword" required />
+          <div className='login-bottom'>
+            <button type="submit">Sign Up</button>
 
-        <button type="submit">Sign Up</button>
-
-        <p>By clicking the Sign Up button, you agree to our <a href='https://ciccc.ca/policy/prior-learning-assessment-policy/'>Terms & Conditions</a> and <a href='https://ciccc.ca/policy/admission-policy/'>Privacy Policy</a></p>
+            <h3>By clicking the Sign Up button, you agree to our <a href='https://ciccc.ca/policy/prior-learning-assessment-policy/'>Terms & Conditions</a> and <a href='https://ciccc.ca/policy/admission-policy/'>Privacy Policy</a></h3>
+          </div>
+        </div>
       </div>
       <div className='footer'>
         <h4>Already have an account? <a href='#'> Login Here</a></h4>
       </div>
     </div>
+
   );
 }
 
